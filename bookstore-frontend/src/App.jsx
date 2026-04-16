ea44d7eae8f0e4281389d4caa3582c18e9c61dc5
Abhineetsahay <abhineetsahay@gmail.com>
2026-04-16 13:05:02 +0530

    complete frontend

--- bookstore-frontend/src/App.jsx
+++ bookstore-frontend/src/App.jsx
@@ -1,20 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import AddBook from "./pages/AddBook";
 import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import { CartProvider } from "./context/CartContext";
 import "./styles/main.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/add" element={<AddBook />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
